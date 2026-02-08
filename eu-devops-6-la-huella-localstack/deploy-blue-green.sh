aae3da383c3501caf9ed01b0e6034398c7797287
manu <maypi72@msn.com>
2026-02-08 17:41:35 +0100

    repair localstack check

--- eu-devops-6-la-huella-localstack/deploy-blue-green.sh
+++ eu-devops-6-la-huella-localstack/deploy-blue-green.sh
@@ -204,7 +204,7 @@ deploy_green() {
 
     #-ESPERAR A QUE LOCALSTACK ESTÉ DISPONIBLE
     echo -e "${BLUE}⏳ Esperando a que LocalStack responda...${NC}"
-    check_health "localstack" "http://localhost:4566/health"
+    check_health "localstack" "http://localhost:4566/_localstack/health > /dev/null"
     
     #-Inicializar recursos aws
     echo -e "${BLUE}⚙️ Inicializando recursos en LocalStack...${NC}"
